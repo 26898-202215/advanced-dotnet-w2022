e2614f0aba923557eda0b8f9c7d6232dae8810c3
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2022-01-11 23:07:34 -0500

    Week01

---
+++ Week01_1_Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week01_1_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

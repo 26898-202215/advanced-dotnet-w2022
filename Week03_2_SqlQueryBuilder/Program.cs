3946f5b966e3e665d327eaa2c7b0db64f3cf275a
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2022-01-31 21:42:52 -0500

    Week 3.2

---
+++ Week03_2_SqlQueryBuilder/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Week03_2_SqlQueryBuilder
+{
+    class Program
+    {
+
+        private static readonly Dictionary<Type, string> typeMap = new Dictionary<Type, string>
+        {
+            { typeof(Guid), "uniqueidentifier NOT NULL"},
+            { typeof(Guid?), "uniqueidentifier NULL"},
+            { typeof(DateTimeOffset), "datetimeoffset(7) NOT NULL"},
+            { typeof(DateTimeOffset?), "datetimeoffset(7) NULL"},
+            { typeof(int), "INT NOT NULL"},
+            { typeof(int?), "INT NULL"},
+            { typeof(double), "decimal(20,0) NOT NULL"},
+            { typeof(double?), "decimal(20,0) NULL"},
+            { typeof(string), "varchar(max) NULL"}
+        };
+        static void Main(string[] args)
+        {
+            // find all the types in the current assembly
+            // that are not abstract classes and are not generic types
+            // and that implement the IDatabaseTable interface
+            var types =
+                typeof(Program).Assembly.ExportedTypes.Where(t => t.IsClass
+                && !t.IsAbstract
+                && !t.IsGenericType
+                && typeof(IDatabaseTable).IsAssignableFrom(t));
+
+            var results = types.Select(GenerateTable);
+
+            foreach (var item in results)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static string GenerateTable(Type type)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine($"CREATE TABLE {type.Name}");
+            builder.AppendLine("(");
+
+            // find all the properties on the given type
+            // that are public instance properties
+            foreach (var propertyInfo in type.GetProperties(BindingFlags.Instance | BindingFlags.Public))
+            {
+                // generate the SQL query portion of each column
+                // based on the property info
+                builder.AppendLine($"\t{GenerateColumn(propertyInfo)}");
+            }
+            builder.AppendLine(")");
+            return builder.ToString();
+        }
+
+        public static string GenerateColumn(PropertyInfo propertyInfo)
+        {
+            return $"{propertyInfo.Name} {typeMap[propertyInfo.PropertyType]},";
+        }
+
+
+    }
+
+    public interface IDatabaseTable
+    {
+
+    }
+
+    public class Owner : IDatabaseTable
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedTime { get; set; }
+
+    }
+
+    public interface IShape : IDatabaseTable
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedTime { get; set; }
+    }
+
+    public abstract class Shape : IShape
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedTime { get; set; }
+        public Guid OwnerId { get; set; }
+
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+    }
+
+    public class Triangle : Shape
+    {
+        public double Base { get; set; }
+        public double Height { get; set; }
+
+    }
+    public class Square : Shape
+    {
+        public double Side { get; set; }
+    }
+}
---
+++ Week03_3_DynamicKeyword/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Week03_3_DynamicKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // dynamic typing is the process of resolving member information at runtime, regardless of type
+            // we can do this using the "dynamic" keyword
+
+            // declare and initialize a dynamic object
+            // using the ExpandoObject class, we can add members to a type a runtime
+            ExpandoObject expandoObject = new ExpandoObject();
+
+            dynamic myObject = new ExpandoObject();
+
+            // by declaring and initializing the value of our property
+            // we have effectively added the property Color, and Size to our object
+            myObject.Color = "Red";
+            myObject.Size = "Large";
+
+            Console.WriteLine(myObject.Color);
+            Console.WriteLine(myObject.Size);
+
+
+            //EX2:
+            dynamic d = new ExpandoObject();
+
+            d.MyProperty = "testing value";
+
+            // by declaring and initializing an action called MyMethod
+            // we have effectively added the method MyMethod to our object
+            d.MyMethod = (Action)(() =>
+            {
+                Console.WriteLine("this is a dynamic action");
+            });
+
+            // let's print out the contents of the property
+            Console.WriteLine(d.MyProperty);
+
+            // let's invoke our method we just created
+            d.MyMethod();
+
+
+
+            //EX3 
+            //****** Exception throwing cases ******
+            dynamic d2 = new object();// this will compile successfully, however will encounter a runtime error
+
+            // Reason: because object does not contain a property called MyProperty
+            // un-comment the below line of code and run the project to see the error
+            // d2.MyProperty = "test";
+
+            dynamic d3 = null; // this will compile successfully, however an exception of type RuntimeBinderException will be thrown
+
+            // Reason: because we cannot perform runtime binding on a null reference, i.e. an object that is null or has not been initialized
+            // un-comment the below line of code and run the project to see the error
+            //d3.MyMethod = (Action)(() =>
+            //{
+            //	Console.WriteLine("this is a dynamic action");
+            //});
+
+        }
+    }
+}

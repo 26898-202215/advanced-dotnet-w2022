5b235f042f0a850680f7ad64506f047ccb90c480
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2022-03-04 00:48:05 -0500

    Week 7

---
+++ Week07_2_WebAPI/Migrations/20220301232725_init.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Week07_2_WebAPI.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 128, nullable: false),
+                    LastName = table.Column<string>(maxLength: 128, nullable: false),
+                    CreationTime = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
---
+++ Week07_2_WebAPI/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week07_2_WebAPI.Models
+{
+    public class Person
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(128)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(128)] 
+        public string LastName { get; set; }
+        [Required]
+        public DateTimeOffset CreationTime { get; set; }
+    }
+}

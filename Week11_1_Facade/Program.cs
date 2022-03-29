77b20f076ed9f64dd7758e072c13a528e74bc1d3
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2022-03-28 23:44:57 -0400

    Week11

---
+++ Week11_1_Facade/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Week11_1_Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting 'Facade' Design Pattern Application!");
+
+            Driver sampleDriver = new Driver("Abc 123 XYZ", "John Doe");
+
+            InsuranceFacade newFacade = new InsuranceFacade();
+            newFacade.SetRate(sampleDriver);
+
+
+        }
+    }
+}
---
+++ Week11_2_Decorator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Week11_2_Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizzaA = new PanPizza();
+            pizzaA = new Cheese(pizzaA);
+            //pizzaA = new Cheese(pizzaA);
+            pizzaA = new Olives(pizzaA);
+
+            Console.WriteLine(pizzaA.GetOrderDetails());
+            Console.WriteLine(pizzaA.GetOrderTotal());
+
+            Pizza pizzaB = new PanPizza();
+            pizzaB = new Cheese(pizzaB);
+
+            Console.WriteLine(pizzaB.GetOrderDetails());
+            Console.WriteLine(pizzaB.GetOrderTotal());
+
+        }
+    }
+}
